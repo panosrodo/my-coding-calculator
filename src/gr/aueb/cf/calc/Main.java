148871a8357c78a1704e2625cf60769e31922c28
panosrodo <brandonroots@hotmail.gr>
2024-12-17 19:34:36 +0200

    mod hotfix. state testing neq zero for denominator

--- src/gr/aueb/cf/calc/Main.java
+++ src/gr/aueb/cf/calc/Main.java
@@ -7,12 +7,7 @@ public class Main {
     }
 
     public static int add(int a, int b) {
-        try {
-            return a / b;
-        } catch (ArithmeticException e) {
-            System.err.println("Error. Denominator must not be zero.");
-            throw e;
-        }
+        if (b == 0) return 0;
     }
     public static int sub(int a, int b) {
         return a - b;
